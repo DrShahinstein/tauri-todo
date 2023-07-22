17c83d0a1d348657ab92563794324374177a953c
DrShahinstein <1taylanozturk@protonmail.com>
2023-07-22 16:18:32 +0300

    Modify rocket build accordingly

--- rocket-server/src/main.rs
+++ rocket-server/src/main.rs
@@ -1,12 +1,23 @@
 #[macro_use]
 extern crate rocket;
 
+mod handlers;
+mod todo;
+
+use handlers::{add_todo, delete_todo, get_todos};
+use std::sync::{Arc, Mutex};
+use todo::Todo;
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Welcome to the Todo App! Use /api to manage todos."
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let todos = Arc::new(Mutex::new(Vec::<Todo>::new()));
+    rocket::build()
+        .manage(todos)
+        .mount("/", routes![index])
+        .mount("/api", routes![get_todos, add_todo, delete_todo])
 }

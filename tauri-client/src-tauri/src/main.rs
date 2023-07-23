1691788c343170daae818c43ef23b582f197645d
DrShahinstein <1taylanozturk@protonmail.com>
2023-07-23 22:35:02 +0300

    Register API functions

--- tauri-client/src-tauri/src/main.rs
+++ tauri-client/src-tauri/src/main.rs
@@ -1,22 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-pub mod commands {
-    pub mod api;
+mod commands {
+    pub mod api {
+        pub mod commands;
+        pub mod funcs;
+    }
 }
-pub mod models {
+
+mod models {
     pub mod todo;
 }
 
-use commands::api::{add_todo, delete_todo, get_todos};
-
-// Find a way to create tauri commands with satisfication of async functions in the program.
-// Obstackles occur within the Tauri app because of awkwards with asyncs.
+use commands::api::commands::{command_add_todo, command_delete_todo, command_get_todos};
 
 fn main() {
     dotenv::dotenv().ok();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![/* ...commands... */])
+        .invoke_handler(tauri::generate_handler![
+            command_get_todos,
+            command_add_todo,
+            command_delete_todo,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
